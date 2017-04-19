59782a7451490fa5a78213d17bdcd0a796df90cd
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-04-18 18:08:39 -0700

    Init commit

---
+++ script.js
@@ -0,0 +1,47 @@
+var BASE_URL = 'http://scottbowlerdev.com/api';
+var API_KEY = '?key=testuser1234';
+
+axios.get(BASE_URL + '/todos' + API_KEY).then(function(resp){
+    var todos = resp.data.todos;
+    var table = $('table tbody');
+
+    // addToDom(todos, table);
+    addToDomNoMap(todos, table);
+});
+
+var addToDom = function(list, container) {
+    console.info('Using Map');
+
+    var tableRows = list.map((item, index) => {
+
+        var tableData = [
+            $(`<td>${index + 1}</td>`),
+            $(`<td>${item.title}</td>`),
+            item.complete ? $(`<td class="text-success">Yes</td>`) : $(`<td class="text-danger">No</td>`)
+        ];
+
+        return $('<tr>').append(tableData);
+    });
+
+    container.append(tableRows);
+};
+
+var addToDomNoMap = function(list, container) {
+    console.info('Using For Loop');
+
+    var tableRows = [];
+
+    for(var i = 0; i < list.length; i++){
+        var item = list[i];
+
+        var tableData = [
+            $(`<td>${i + 1}</td>`),
+            $(`<td>${item.title}</td>`),
+            item.complete ? $(`<td class="text-success">Yes</td>`) : $(`<td class="text-danger">No</td>`)
+        ];
+
+        tableRows.push($('<tr>').append(tableData));
+    }
+
+    container.append(tableRows);
+};
---
+++ script_es6.js
@@ -0,0 +1,24 @@
+const BASE_URL = 'http://scottbowlerdev.com/api';
+const API_KEY = '?key=testuser1234';
+
+axios.get(`${BASE_URL}/todos${API_KEY}`).then(resp => {
+    const { todos } = resp.data;
+    const table = $('table tbody');
+
+    addToDom(todos, table);
+});
+
+const addToDom = (list, container) => {
+    const tableRows = list.map((item, index) => {
+
+        const tableData = [
+            $(`<td>${index + 1}</td>`),
+            $(`<td>${item.title}</td>`),
+            item.complete ? $(`<td class="text-success">Yes</td>`) : $(`<td class="text-danger">No</td>`)
+        ];
+
+        return $('<tr>').append(tableData);
+    });
+
+    container.append(tableRows);
+};
